66376b36eef2f82425c33c6b4093f2e9768cd990
a <a>
2023-12-13 20:06:32 -0500

    remove blackarch; add more basics

--- device-nibbler/setup.sh
+++ device-nibbler/setup.sh
@@ -5,30 +5,36 @@ set -x
 
 # === BLACK ARCH
 # Add blackarch to mirror list. Nice to have for general infosec tools.
-curl -O https://blackarch.org/strap.sh
-TARGET='5ea40d49ecd14c2e024deecf90605426db97ea0c' # https://blackarch.org/downloads.html, section: 'Installing on top of Arch Linux'. This is the correct hash; the one in the black arch install instructions is wrong
-HASH=$(sha1sum strap.sh | cut -d ' ' -f 1)
-if [ ! "$TARGET" = "$HASH" ]; then
-	echo 'blackarch script hash does not match'
-	exit 1;
-fi
-chmod +x strap.sh
-./strap.sh
+#curl -O https://blackarch.org/strap.sh
+# TODO update this hash
+#TARGET='5ea40d49ecd14c2e024deecf90605426db97ea0c' # https://blackarch.org/downloads.html, section: 'Installing on top of Arch Linux'. This is the correct hash; the one in the black arch install instructions is wrong
+#HASH=$(sha1sum strap.sh | cut -d ' ' -f 1)
+#if [ ! "$TARGET" = "$HASH" ]; then
+#	echo 'blackarch script hash does not match'
+#	exit 1;
+#fi
+#chmod +x strap.sh
+#./strap.sh
 
 # Update index including blackarch
 pacman -Syyu
 
 # Install some essentials
 pacman -Syy --needed neovim \
+	alacritty \
 	base-devel \
 	go \
+	man \
 	rustup \
+	tig \
 	tmux \
 	keepassxc \
-	ripgrep
+	ripgrep \
+	ufw
 	# npm \
 	# nvm \
 	# yarn \
+	
 
 # hacking/networking tools
 # pacman -Syy --needed wireshark-qt \
@@ -41,7 +47,7 @@ pacman -Syy --needed neovim \
 # uname -a
 # pacman -Syy --needed virtualbox virtualbox-guest-iso 
 
-# TODO: Add yay for AUR packages
+# TODO: Add yay or pikaur for AUR packages
 
 # cleanup
 pacman -Sc # clean
@@ -50,3 +56,7 @@ pacman -Sc # clean
 rustup toolchain install nightly
 rustup target add wasm32-unknown-unknown
 rustup target add wasm32-unknown-unknown --toolchain nightly
+
+# Manjaro kruft
+# pacman -R kdeconnect
+# pacman -R kpeoplevcard
